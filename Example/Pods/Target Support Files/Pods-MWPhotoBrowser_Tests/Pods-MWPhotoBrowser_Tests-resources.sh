79d4eb97815f4c10004d913f8c5660644e1a97b6
TomJack1 <445765368@qq.com>
2019-07-23 09:57:44 +0800

    s

--- Example/Pods/Target Support Files/Pods-MWPhotoBrowser_Tests/Pods-MWPhotoBrowser_Tests-resources.sh
+++ Example/Pods/Target Support Files/Pods-MWPhotoBrowser_Tests/Pods-MWPhotoBrowser_Tests-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/CYLMWPhotoBrowser/CYLMWPhotoBrowser.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/CYLMWPhotoBrowser/MWPhotoBrowser.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/CYLMWPhotoBrowser/CYLMWPhotoBrowser.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/CYLMWPhotoBrowser/MWPhotoBrowser.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
